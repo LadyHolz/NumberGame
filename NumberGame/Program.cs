dd7926839620e931574c557501522fab46f744f2
Gemma Holz <Gemma.Holz@balticapprenticeships.com>
2022-05-22 13:02:42 +0100

    Add project files.

---
+++ NumberGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number: ");
+            string strNumber = Console.ReadLine();
+            int intNumber = int.Parse(strNumber);
+
+            if (intNumber == 2)
+            {
+                Console.WriteLine(intNumber + "is correct, you won");
+            }
+            else
+            {
+                Console.WriteLine(intNumber + " " + "is incorrect, you lost");
+            }
+        }
+    }
+}
